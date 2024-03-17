fcbd7e7735dd68db08758101e75029530c288171
Jiyad-M <jiyad8201@gmail.com>
2024-03-18 00:28:15 +0530

    Login ok

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Header from '../../components/header/Header'
+import React from "react";
+import Header from "../../components/header/Header"; 
+import './Login.css'
 
 function Login() {
   return (
-    <div>
-      <Header/>
-        
+    <div className="login">
+      <Header />
+      <div className="l-form">
+        <form action="" method="post">
+          <input type="text" placeholder="Email / Mobile no" />
+          <input type="text" placeholder="passsword" />
+          <button type="submit">Login</button>
+          <h3>you have no account</h3>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
